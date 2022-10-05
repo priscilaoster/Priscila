e2c2f19324ebbdbece0ede8a3e48b40d4dbb1be6
priscila <ostertag.priscila.epet20@gmail.com>
2022-10-05 15:30:31 -0300

    trabajo de js terminado

---
+++ script.js
@@ -0,0 +1,28 @@
+//#region 
+var nombre ="priscila";
+let apellido = 'ostertag';
+const edad=
+18;
+var peso= 44;
+var altura= 48;
+//#endregion
+
+//#region
+console.log(nombre);
+console.log (apellido);
+console.log (edad);
+console.log (altura);
+//#endregion
+//#region
+var inicio=0;
+var  fin=[10];
+for (inicio of fin){
+    console.log (nombre);
+    console.log (apellido);
+    console.log (edad);
+    console.log (peso);
+    console.log (altura);
+}
+var formula = (peso/(altura*altura))
+console.log (formula);
+//#endregion
